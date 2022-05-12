9ca7572a85fcdeab1e1f26e495dfc150a529af69
unknown <azer.maslow@gmail.com>
2022-05-12 17:05:03 +0400

    development

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {NgSelectConfig} from "@ng-select/ng-select";
 
 @Component({
   selector: 'app-home',
@@ -6,8 +7,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  selectedCar: number | undefined;
 
-  constructor() { }
+  cars = [
+    { id: 1, name: 'London' },
+    { id: 2, name: 'Brighton' },
+    { id: 3, name: 'Edinburgh' },
+    { id: 4, name: 'Manchester' },
+  ];
+  constructor(private config: NgSelectConfig) {
+    this.config.notFoundText = 'Custom not found';
+    this.config.appendTo = 'body';
+    // set the bindValue to global config when you use the same
+    // bindValue in most of the place.
+    // You can also override bindValue for the specified template
+    // by defining `bindValue` as property
+    // Eg : <ng-select bindValue="some-new-value"></ng-select>
+    this.config.bindValue = 'value';
+  }
 
   ngOnInit(): void {
   }
